9e5a2d8a8b73100b7c154413b89e23eee7c06546
Alex C <alex.christianson.05@gmail.com>
2021-12-23 14:36:42 -0600

    added models

---
+++ models/Users.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Please enter a valid email"]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Users'
+            }
+        ]
+    },
+    {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+    }
+)
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const Users = model('Users', UserSchema);
+
+module.exports = Users;
